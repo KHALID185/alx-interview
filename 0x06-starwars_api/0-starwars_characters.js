971d26ea13e5d7d613b52afa1e688139edc75b8d
KHALID185 <khalid.1998.taouti@gmail.com>
2024-11-12 13:42:20 -0800

    alx interviw 6

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,53 +2,22 @@
 
 const request = require('request');
 
-/**
- * Promisifies the request function to use with async/await
- * @param {string} url - The URL to make the request to
- * @returns {Promise} Promise object represents the request
- */
-function makeRequest(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        reject(error || `Status code: ${response.statusCode}`);
-      }
-      resolve(JSON.parse(body));
-    });
-  });
-}
-
-/**
- * Gets and prints all character names for a specific Star Wars movie
- * @param {string} movieId - The ID of the movie to get characters from
- */
-async function printMovieCharacters(movieId) {
-  try {
-    // Get movie data
-    const baseUrl = 'https://swapi-api.hbtn.io/api';
-    const movieData = await makeRequest(`${baseUrl}/films/${movieId}`);
-    const characterUrls = movieData.characters;
-
-    // Get all character data in parallel
-    const characterPromises = characterUrls.map(url => makeRequest(url));
-    const characters = await Promise.all(characterPromises);
-
-    // Print character names in order
-    characters.forEach(character => console.log(character.name));
-  } catch (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-}
-
-// Get movie ID from command line argument
 const movieId = process.argv[2];
 
-// Validate movie ID
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Execute main function
-printMovieCharacters(movieId);
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
