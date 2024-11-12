344445c777e3cb5b651e8721082cad777b991259
KHALID185 <khalid.1998.taouti@gmail.com>
2024-11-12 14:11:53 -0800

    sratwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,24 @@
 #!/usr/bin/node
-/**
- * Script that prints all characters of a Star Wars movie
- * Usage: ./0-starwars_characters.js <movie_id>
- */
+
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-  if (error || response.statusCode !== 200) {
-    console.error('Error:', error);
-    process.exit(1);
+  if (!error) {
+    const results = JSON.parse(body);
+    printCharacters(results.characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  if (index === characters.length) {
+    return;
   }
 
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-  let count = 0;
-
-  const printCharacter = function (characterUrl) {
-    request(characterUrl, function (error, response, body) {
-      if (error || response.statusCode !== 200) {
-        console.error('Error:', error);
-        process.exit(1);
-      }
-      
-      const character = JSON.parse(body);
-      console.log(character.name);
-      count++;
-
-      if (count === characters.length) {
-        process.stdout.write('');  // Ensure no trailing newline
-      }
-    });
-  };
-
-  characters.forEach(function (characterUrl) {
-    printCharacter(characterUrl);
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    }
   });
-});
+}
