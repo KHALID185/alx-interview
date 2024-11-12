3b678ce73fe87249c5f8d157905195653c0fb7ec
KHALID185 <khalid.1998.taouti@gmail.com>
2024-11-12 14:01:52 -0800

    sratwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -24,31 +24,62 @@ function makeRequest(url) {
  */
 async function printMovieCharacters(movieId) {
   try {
-    // Get movie data
     const baseUrl = 'https://swapi-api.hbtn.io/api';
     const movieData = await makeRequest(`${baseUrl}/films/${movieId}`);
     const characterUrls = movieData.characters;
 
-    // Get all character data in parallel
     const characterPromises = characterUrls.map(url => makeRequest(url));
     const characters = await Promise.all(characterPromises);
 
-    // Print character names in order
-    characters.forEach(character => console.log(character.name));
+    // Print each character name with proper formatting
+    characters.forEach((character, index) => {
+      const name = character.name;
+      // For the last character, don't add a newline
+      if (index === characters.length - 1) {
+        process.stdout.write(name);
+      } else {
+        process.stdout.write(name + '\n');
+      }
+    });
   } catch (error) {
     console.error('Error:', error);
     process.exit(1);
   }
 }
 
-// Get movie ID from command line argument
-const movieId = process.argv[2];
+// Debug function to show hidden characters
+function debugOutput(movieId) {
+  return new Promise((resolve) => {
+    let output = '';
+    const oldWrite = process.stdout.write;
+    process.stdout.write = (function(write) {
+      return function(string, encoding, fd) {
+        output += string;
+        write.apply(process.stdout, arguments);
+      };
+    })(process.stdout.write);
 
-// Validate movie ID
+    printMovieCharacters(movieId).then(() => {
+      process.stdout.write = oldWrite;
+      console.log('\n\nDEBUG INFO:');
+      console.log('Length:', output.length);
+      console.log('Characters (showing hidden):');
+      for (let i = 0; i < output.length; i++) {
+        const char = output[i];
+        const code = output.charCodeAt(i);
+        console.log(`Position ${i}: '${char}' (ASCII: ${code})`);
+      }
+      resolve();
+    });
+  });
+}
+
+const movieId = process.argv[2];
 if (!movieId) {
   console.error('Please provide a movie ID');
   process.exit(1);
 }
 
-// Execute main function
-printMovieCharacters(movieId);
+// Comment out this line and uncomment the one below for normal usage
+// printMovieCharacters(movieId);
+debugOutput(movieId);
