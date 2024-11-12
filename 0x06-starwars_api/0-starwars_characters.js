38e37273d926b200c19d30f39bb179b89a6f9232
KHALID185 <khalid.1998.taouti@gmail.com>
2024-11-12 14:08:10 -0800

    sratwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,57 +1,46 @@
 #!/usr/bin/node
-
+/**
+ * Script that prints all characters of a Star Wars movie
+ * Usage: ./0-starwars_characters.js <movie_id>
+ */
 const request = require('request');
 
-/**
- * Promisifies the request function to use with async/await
- * @param {string} url - The URL to make the request to
- * @returns {Promise} Promise object represents the request
- */
-function makeRequest(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        reject(error || `Status code: ${response.statusCode}`);
-      }
-      resolve(JSON.parse(body));
-    });
-  });
-}
-
-/**
- * Gets and prints all character names for a specific Star Wars movie
- * @param {string} movieId - The ID of the movie to get characters from
- */
-async function printMovieCharacters(movieId) {
-  try {
-    const baseUrl = 'https://swapi-api.hbtn.io/api';
-    const movieData = await makeRequest(`${baseUrl}/films/${movieId}`);
-    const characterUrls = movieData.characters;
-
-    const characterPromises = characterUrls.map(url => makeRequest(url));
-    const characters = await Promise.all(characterPromises);
-
-    // Print characters names
-    characters.forEach((character, index) => {
-      if (index === characters.length - 1) {
-        // Last character - no newline
-        process.stdout.write(character.name);
-      } else {
-        console.log(character.name);
-      }
-    });
-  } catch (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-}
-
-// Get movie ID from command line argument
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Please provide a movie ID');
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
   process.exit(1);
 }
 
-// Execute main function
-printMovieCharacters(movieId);
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error || response.statusCode !== 200) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+  let count = 0;
+
+  const printCharacter = function (characterUrl) {
+    request(characterUrl, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.error('Error:', error);
+        process.exit(1);
+      }
+      
+      const character = JSON.parse(body);
+      console.log(character.name);
+      count++;
+
+      if (count === characters.length) {
+        process.stdout.write('');  // Ensure no trailing newline
+      }
+    });
+  };
+
+  characters.forEach(function (characterUrl) {
+    printCharacter(characterUrl);
+  });
+});
