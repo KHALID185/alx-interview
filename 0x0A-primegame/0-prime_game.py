c96aed7f1968ae2ab05b6e25241e741a6f2c2bd8
KHALID185 <khalid.1998.taouti@gmail.com>
2024-12-11 06:57:21 -0800

    primeee

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,71 @@
 #!/usr/bin/python3
+"""
+Prime Game Problem: Optimized Solution for Maria and Ben's Game
+"""
 
 
-def sieve_primes(n):
-    """Generate prime numbers up to n using Sieve of Eratosthenes"""
-    prime = [True] * (n + 1)
-    prime[0] = prime[1] = False
+def sieve_of_eratosthenes(n):
+    """
+    Efficiently generate prime numbers up to n using Sieve of Eratosthenes
+    Args:
+        n (int): Upper limit for generating primes
+    Returns:
+        list: Primes numbers up to n
+    """
+    # Initialize boolean array for prime checking
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
 
+    # Mark non-prime numbers
     for i in range(2, int(n**0.5) + 1):
-        if prime[i]:
-            for j in range(i*i, n + 1, i):
-                prime[j] = False
+        if is_prime[i]:
+            # Mark multiples of prime as non-prime
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
 
-    return prime
+    # Collect prime numbers
+    return [num for num in range(2, n+1) if is_prime[num]]
+
+
+def count_primes_and_moves(n):
+    """
+    Determine the number of primes and game outcome for a single round
+    Args:
+        n (int): Upper limit of the game range
+    Returns:
+        bool: True if Ben wins, False if Maria wins
+    """
+    # Get primes in the range
+    primes = sieve_of_eratosthenes(n)
+
+    # Game outcome depends on number of primes
+    return len(primes) % 2 == 0
 
 
 def isWinner(x, nums):
-    """Determine the winner of Prime Game
-
+    """
+    Determine the overall winner of multiple rounds of Prime Game
     Args:
         x (int): Number of game rounds
-        nums (list): Maximum numbers for each round
-
+        nums (list): Upper limits for each round
     Returns:
-        str or None: Winner of the most rounds
+        str/None: Name of the winner or None if no clear winner
     """
+    # Input validation
     if not x or not nums or x != len(nums):
         return None
 
+    # Track wins for Maria and Ben
     maria_wins = ben_wins = 0
 
+    # Play each round
     for n in nums:
-        primes = sieve_primes(n)
-        prime_count = sum(primes[2:n+1])
-
-        # Determine winner based on prime count
-        if prime_count % 2 == 0:
+        if count_primes_and_moves(n):
             ben_wins += 1
         else:
             maria_wins += 1
 
+    # Determine overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
